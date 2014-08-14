94f697a3658c63649041a8ff36cc739d51e75f63
DrJia <jiabinjlu@gmail.com>
2014-08-14 10:52:08 +0800

    init

---
+++ LogCollectorExample/src/com/jiabin/logcollectorexample/MyApplication.java
@@ -0,0 +1,14 @@
+package com.jiabin.logcollectorexample;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	
+}
